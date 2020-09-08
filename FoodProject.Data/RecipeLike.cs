e6c8bda06a42142e2a70c50ccbdfade191771d75
Mihail Karlovski <mihailkarlovski@gmail.com>
2020-09-08 18:07:36 +0200

    Update RecipeLike.cs

--- FoodProject.Data/RecipeLike.cs
+++ FoodProject.Data/RecipeLike.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FoodProject.Data
 {
-    class RecipeLike
+    public class RecipeLike
     {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+
+
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+
+
     }
 }
