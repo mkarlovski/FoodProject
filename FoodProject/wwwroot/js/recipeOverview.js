899cf558e88ba9eb51b682ec4c9f6278a89c4181
Mihail Karlovski <mihailkarlovski@gmail.com>
2020-09-28 14:48:57 +0200

    changed js for like button

---
+++ FoodProject/wwwroot/js/recipeOverview.js
@@ -0,0 +1,45 @@
+﻿
+
+function RecipeLike(recipeId) {
+    axios.post('https://localhost:44319/RecipeLikes/Like', {
+        recipeId: recipeId
+    })
+        .then(function () {
+            //var removeLikeBtn = document.getElementById("btnAddLike");
+
+            //if (!removeLikeBtn.classList.contains("displayNone")) {
+            //    DecreaseEngagementCount("dislikeCountContainer");
+            //    SwitchDisplay("btnAddDislike", "btnRemoveDislike");
+            //}
+
+
+            SwitchDisplay("btnRemoveLike", "btnAddLike");
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
+}
+
+
+
+function RemoveRecipeLike(recipeId) {
+    axios.post('https://localhost:44319/RecipeLikes/RemoveLike', {
+        recipeId: recipeId
+    })
+        .then(function () {
+           
+            SwitchDisplay("btnAddLike", "btnRemoveLike");
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
+}
+
+
+function SwitchDisplay(showElement, hideElement) {
+    var showEl = document.getElementById(showElement);
+    var hideEl = document.getElementById(hideElement);
+
+    showEl.classList.remove("displayNone");
+    hideEl.classList.add("displayNone");
+}
